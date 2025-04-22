07b4735a7eb823d84afcc16779fc31b523227d38
merthasanaslan <merthasanaslan325@gmail.com>
2025-04-22 06:51:15 +0300

    renamed some files, added piece class, added shapes_config.py

--- class/shapes.py
+++ class/shapes.py
@@ -1,5 +1,38 @@
-class tetris_piece(piece):
-    def __init__(self, x, y, shape):
+
+
+from config.shapes_config import pieces
+"""
+This game will be original tetris-like
+height = 20, width = 10
+
+tetris symbols:
+    Z_reverse - green
+    Z - red
+    l - cyan
+    O - yellow
+    L_reverse - blue
+    L - orange
+    T - purple
+"""
+
+class piece():
+    def __init__(self, x, y, shape, shape_name, shape_color):
         self.x = x
         self.y = y
         self.shape = shape
+        self.name = shape_name
+        self.color = shape_color
+        self.rotation = 0
+
+    #This function will return current form of piece
+    def get_current(self):
+        return self.shape[self.rotation]
+    
+    #This function will rotate the piece
+    def rotate(self):
+        self.rotation = (self.rotation + 1) % len(self.shape)
+
+
+    
+
+
